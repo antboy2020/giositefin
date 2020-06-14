c2508d492f2e29afed8909b11b81c281ff652c4d
anthony then <anthonythen@Anthonys-MBP.lan1>
2020-06-14 19:36:14 -0400

    changes

--- app.js
+++ app.js
@@ -26,9 +26,46 @@ app.set('view engine', 'ejs');
 app.use(setPassport.passport.initialize());
 app.use(setPassport.passport.session());
 
+const url = require('url');
 // @route GET /
 // @desc Loads form
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+  if (queryObject['session_id']) {
+    let stripe = require('stripe')('sk_test_ztd0iQr35Jsk2fcRHPYUWa9F00AAE3Dlob');
+
+    let session = await stripe.checkout.sessions.retrieve(queryObject['session_id']);
+    let customer = await stripe.customers.retrieve(session.customer);
+    //send email to customer with thanks and order number
+    //send email to gio with the customer email the items they purchased and the shipping addresss
+    //send email to me to let me know the customers used the account
+    if (customer && customer.email) {
+      var nodemailer = require('nodemailer');
+
+      var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'anthony.then95@gmail.com',
+          pass: 'Astdlr.2020gsuite'
+        }
+      });
+
+      var mailOptions = {
+        from: 'anthony.then95@gmail.com',
+        to: 'anthony.then95@rocketmail.com',
+        subject: 'Sending Email using Node.js',
+        text: 'That was easy!'
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+    }
+  }
   let data = { title: "junk1", authenticated: req.session.authenticated, cart: req.session.cart };
   mongooseJS.StoreItem.find({ featured: true }, function (err, fileInfo) {
     if (err) {
@@ -101,21 +138,23 @@ app.get('/products', (req, res) => {
             ) {
               file.isImage = true;
               let thisFileInfo = fileInfo.find((info) => info.filename == file.filename);
-              file.price = thisFileInfo.price;
-              file.description = thisFileInfo.description;
-              file.xs = thisFileInfo.xs;
-              file.s = thisFileInfo.s;
-              file.m = thisFileInfo.m;
-              file.l = thisFileInfo.l;
-              file.xl = thisFileInfo.xl;
-              file.type = thisFileInfo.type;
-              file.ml30 = thisFileInfo.ml30;
-              file.ml50 = thisFileInfo.ml50;
-              file.ml100 = thisFileInfo.ml100;
-              file.ml250 = thisFileInfo.ml250;
-              file.g100 = thisFileInfo.g100;
-              file.wholecount = thisFileInfo.wholecount;
-              file.featured = thisFileInfo.featured;
+              if (thisFileInfo) {
+                file.price = thisFileInfo.price;
+                file.description = thisFileInfo.description;
+                file.xs = thisFileInfo.xs;
+                file.s = thisFileInfo.s;
+                file.m = thisFileInfo.m;
+                file.l = thisFileInfo.l;
+                file.xl = thisFileInfo.xl;
+                file.type = thisFileInfo.type;
+                file.ml30 = thisFileInfo.ml30;
+                file.ml50 = thisFileInfo.ml50;
+                file.ml100 = thisFileInfo.ml100;
+                file.ml250 = thisFileInfo.ml250;
+                file.g100 = thisFileInfo.g100;
+                file.wholecount = thisFileInfo.wholecount;
+                file.featured = thisFileInfo.featured;
+              }
             } else {
               file.isImage = false;
             }
@@ -126,6 +165,11 @@ app.get('/products', (req, res) => {
     }
   });
 });
+
+app.get('/booking', (req, res) => {
+  let data = { cart: req.session.cart };
+  res.render('booking', { data: data });
+})
 
 // @route POST /upload
 // @desc  Uploads file to DB
@@ -198,45 +242,83 @@ app.get('/product/:filename', (req, res) => {
     if (err) {
       res.render('index', { file: false, data: data });
     } else {
-      mongooseJS.gfs.files.find({ filename: req.params.filename }).toArray((err, files) => {
+      mongooseJS.gfs.files.find({ filename: req.params.filename }).toArray(async (err, files) => {
         let file = files.filter(file => !file.filename.includes("secondary"));
-        let secondaryFiles = files.filter(file => file.filename.includes("secondary"));
-          // Check if files
-          if (!file || file.length === 0) {
-            res.render('index', { files: false, data: data });
-          } else {
-            file.map(file => {
-              if (
-                file.contentType === 'image/jpeg' ||
-                file.contentType === 'image/png'
-              ) {
-                file.isImage = true;
-                file.price = fileInfo[0].price;
-                file.description = fileInfo[0].description;
-                file.xs = fileInfo[0].xs;
-                file.s = fileInfo[0].s;
-                file.m = fileInfo[0].m;
-                file.l = fileInfo[0].l;
-                file.xl = fileInfo[0].xl;
-                file.type = fileInfo[0].type;
-                file.ml30 = fileInfo[0].ml30;
-                file.ml50 = fileInfo[0].ml50;
-                file.ml100 = fileInfo[0].ml100;
-                file.ml250 = fileInfo[0].ml250;
-                file.g100 = fileInfo[0].g100;
-                file.wholecount = fileInfo[0].wholecount;
-                file.featured = fileInfo[0].featured;
-              } else {
-                file.isImage = false;
-              }
-            });
-            res.render('product', { file: file[0], secondaryFiles: secondaryFiles, data: data });
-          }
+        // let secondaryFiles = files.filter(file => file.filename.includes("secondary"));
+        // Check if files
+        if (!file || file.length === 0) {
+          res.render('index', { files: false, data: data });
+        } else {
+          file.map(file => {
+            if (
+              file.contentType === 'image/jpeg' ||
+              file.contentType === 'image/png'
+            ) {
+              file.isImage = true;
+              file.price = fileInfo[0].price;
+              file.description = fileInfo[0].description;
+              file.xs = fileInfo[0].xs;
+              file.s = fileInfo[0].s;
+              file.m = fileInfo[0].m;
+              file.l = fileInfo[0].l;
+              file.xl = fileInfo[0].xl;
+              file.type = fileInfo[0].type;
+              file.ml30 = fileInfo[0].ml30;
+              file.ml50 = fileInfo[0].ml50;
+              file.ml100 = fileInfo[0].ml100;
+              file.ml250 = fileInfo[0].ml250;
+              file.g100 = fileInfo[0].g100;
+              file.wholecount = fileInfo[0].wholecount;
+              file.featured = fileInfo[0].featured;
+            } else {
+              file.isImage = false;
+            }
+          });
+          res.render('product', { file: file[0], data: data });
+        }
 
       });
     }
   });
 });
+
+app.get('/billing', async (req, res) => {
+  console.log("hello");
+  let total = cartTotal(req).total;
+  let sessionId = await readyCheckout(total);
+  res.render('billing', { sessionId: sessionId });
+});
+
+
+
+function cartTotal(req) {
+  let total = 0;
+  let clothesTotal = 0;
+  let barberTotal = 0;
+  let tax = 0;
+  let count = 0;
+  let shipping = 0;
+  if (req.session.cart) {
+    for (let [key, val] of Object.entries(req.session.cart)) {
+      count += val.count;
+    }
+    shipping = count == 1 ? 5.95 : 7.95;
+    for (let [key, value] of Object.entries(req.session.cart)) {
+      if (value.type == "clothing") {
+        clothesTotal += value.price * value.count;
+      } else if (value.type == "barbering") {
+        barberTotal += value.price;
+      }
+    }
+  }
+  if (clothesTotal > 110) {
+    tax = (clothesTotal + barberTotal) * .04;
+  } else {
+    tax = (barberTotal) * .04;
+  }
+  total = clothesTotal + barberTotal + tax + shipping;
+  return { total: total, tax: tax, shipping: shipping };
+}
 
 //reoute to update the count that is remaining in stock for store item
 app.post('/updateCount/:filename/:size/:count', (req, res) => {
@@ -260,52 +342,129 @@ app.post('/updateFeatured/:filename/:featured', (req, res) => {
   });
 });
 
-// TODO route to checkout
-app.get('/billing', (req, res) => {
-  let total = 0;
-  let clothesTotal = 0;
-  let barberTotal = 0;
-  let tax = 0;
-  let shipping = 0;
-  if(req.session.cart){
-   shipping = Object.keys(req.session.req).length == 1 ? 5.95 : 7.95;
-  for (let[key, value] of Object.entries(req.session.cart)) {
-    if(value.type == "clothing"){
-      clothesTotal += value.price * value.count
-    } else if (value.type == "barbering") {
-      barberTotal += value.price
+async function readyCheckout(total) {
+  let itemNotFound = true;
+  let legeitCartName = "legeitcheckout";
+  let sessionId = "";
+  let productList = await stripe.products.list({ active: true });
+  let products = productList.data;
+  if (products) {
+    for (product in products) {
+      if (products[product].name == legeitCartName) {
+        itemNotFound = false;
+        const session = await stripe.checkout.sessions.create({
+          payment_method_types: ['card'],
+          line_items: [{
+            price_data: {
+              currency: 'usd',
+              product: products[product].id,
+              unit_amount: total * 100,
+            },
+            quantity: 1,
+          }],
+          mode: 'payment',
+          success_url: 'http://localhost:5000?session_id={CHECKOUT_SESSION_ID}',
+          cancel_url: 'http://localhost:5000',
+          shipping_address_collection: {
+            allowed_countries: ['US', 'CA'],
+          },
+        });
+        sessionId = session.id;
+      }
     }
-  }}
-  if(clothesTotal > 110) {
-    tax = (clothesTotal + barberTotal) * .04
-  } else {
-    tax = (barberTotal) * .04
   }
-  res.render('billing', { data: req.session.cart, tax: tax, shipping: shipping });
-});
+  if (itemNotFound) {
+    const product = await stripe.products.create({
+      name: legeitCartName,
+    });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [{
+        price_data: {
+          currency: 'usd',
+          product: product.id,
+          unit_amount: total * 100,
+        },
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: 'http://localhost:5000',
+      cancel_url: 'https://localhost:5000',
+      shipping_address_collection: {
+        allowed_countries: ['US', 'CA'],
+      },
+    });
+    sessionId = session.id;
+    // const price = await stripe.prices.create({
+    //   product: product.id,
+    //   unit_amount: req.params.cartTotal * 100,
+    //   currency: 'usd',
+    // });
+  }
+  return sessionId;
+}
+
+// TODO route to checkout
+// app.get('/billing/:cartTotal', async (req, res) => {
+//   let itemnotfound = true;
+//   let legeitcartname = "legeitcheckout";
+//   let productlist = await stripe.products.list({ active: true });
+//   if (productlist.data) {
+//     productlist.data.foreach((product)=> {
+//       if(product.name == legeitcartname) {
+//         itemnotfound = false;
+//         const session = await stripe.checkout.sessions.create({
+//           payment_method_types: ['card'],
+//           line_items: [{
+//             price_data: {
+//               currency: 'usd',
+//               product: product.id,
+//               unit_amount: 2000,
+//             },
+//           }],
+//           mode: 'payment',
+//           success_url: 'https://example.com/success?session_id={checkout_session_id}',
+//           cancel_url: 'https://example.com/cancel',
+//         });
+//       }
+//     })
+//   }
+//   if (itemnotfound) {
+//     const product = await stripe.products.create({
+//       name: legeitcartname,
+//     });
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ['card'],
+//       line_items: [{
+//         price_data: {
+//           currency: 'usd',
+//           product: product.id,
+//           unit_amount: 2000,
+//         },
+//       }],
+//       mode: 'payment',
+//       success_url: 'https://example.com/success?session_id={checkout_session_id}',
+//       cancel_url: 'https://example.com/cancel',
+//     });
+//     // const price = await stripe.prices.create({
+//     //   product: product.id,
+//     //   unit_amount: req.params.carttotal * 100,
+//     //   currency: 'usd',
+//     // });
+//   }
+//   res.render('billing', { data: req.session.cart });
+// });
 
 // route to update and view cart
 app.get('/updateCart', (req, res) => {
-  let total = 0;
-  let clothesTotal = 0;
-  let barberTotal = 0;
-  let tax = 0;
-  let shipping = 0;
-  if(req.session.cart){
-   shipping = Object.keys(req.session.req).length == 1 ? 5.95 : 7.95;
-  for (let[key, value] of Object.entries(req.session.cart)) {
-    if(value.type == "clothing"){
-      clothesTotal += value.price * value.count
-    } else if (value.type == "barbering") {
-      barberTotal += value.price
-    }
-  }}
-  if(clothesTotal > 110) {
-    tax = (clothesTotal + barberTotal) * .04
+  if (req.session.cart && Object.keys(req.session.cart).length > 0) {
+
+    let cartPricingDetails = cartTotal(req);
+    res.render('updatecart', { data: req.session.cart, tax: cartPricingDetails.tax, shipping: cartPricingDetails.shipping });
   } else {
-    tax = (barberTotal) * .04
+
+    res.render('updatecart', { data: req.session.cart, tax: null, shipping: null });
   }
-  res.render('updatecart', { data: req.session.cart, tax: tax, shipping: shipping });
 });
 
 app.get('/updateCart/:filename', (req, res) => {
